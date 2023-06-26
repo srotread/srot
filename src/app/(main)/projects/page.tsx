926297830961ca87859f4df4dd05e9fdc24320a1
Neesh <avaneeshsamsi@gmail.com>
2023-06-26 14:47:40 +0530

    feat: Dynamic projects page data from Keystatic

--- src/app/(main)/projects/page.tsx
+++ src/app/(main)/projects/page.tsx
@@ -1,54 +1,89 @@
-import type { FC } from "react"
 import Card from "@/components/Card"
 
-const Projects: FC = () => {
+import reader from "@/lib/keystatic"
+import { KeystaticContentNotFoundError } from "@/lib/exceptions"
+
+async function getPageData() {
+  const config = await reader.singletons.config.read()
+  const projectspage = await reader.singletons.projectspage.read({
+    resolveLinkedFiles: true,
+  })
+  const projects = await reader.collections.projects.all()
+
+  if (!config) {
+    throw new KeystaticContentNotFoundError("Site Settings")
+  }
+  if (!projectspage) {
+    throw new KeystaticContentNotFoundError("Projects Page singleton")
+  }
+  if (!projects) {
+    throw new KeystaticContentNotFoundError("Projects collection")
+  }
+
+  const { headline, subheadline, metaTitle, metaDescription } = projectspage
+
+  return {
+    meta: {
+      title: `${metaTitle} | ${config.siteTitle}`,
+      description: metaDescription,
+    },
+    page: {
+      headline,
+      subheadline,
+      projects,
+    },
+  }
+}
+
+export async function generateMetadata() {
+  const { title, description } = (await getPageData()).meta
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+    },
+    twitter: {
+      title,
+      description,
+    },
+    alternates: {
+      canonical: "/projects",
+    },
+  }
+}
+
+const Projects = async (): Promise<JSX.Element> => {
+  const { headline, subheadline, projects } = (await getPageData()).page
+
   return (
     <main className="bg-light px-col-outer py-16 text-dark lg:px-col-inner lg:py-36">
       <h1 className="text-4xl font-bold underline decoration-accent md:text-5xl 2xl:text-6xl 3xl:text-7xl">
-        Our Projects
+        {headline}
       </h1>
 
       <p className="my-9 text-lg 2xl:my-16 2xl:text-xl 3xl:text-2xl">
-        Your support helps fund projects like:
+        {subheadline}
       </p>
 
       <div className="grid gap-x-16 gap-y-14 lg:grid-cols-1 xl:grid-cols-2 2xl:gap-x-32 2xl:gap-y-20">
-        <Card
-          type="Project"
-          img="/hero.png"
-          alt="Srot READ Project"
-          title="Srot READ Project"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deleniti incidunt vero facere vitae quod commodi amet? Animi, esse molestias."
-          btnType="Primary"
-          slug="test-project"
-        />
-        <Card
-          type="Project"
-          img="/hero.png"
-          alt="Srot READ Project"
-          title="Srot READ Project"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          btnType="Secondary"
-          slug="test-project"
-        />
-        <Card
-          type="Project"
-          img="/hero.png"
-          alt="Srot READ Project"
-          title="Srot READ Project"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deleniti incidunt vero facere vitae quod commodi amet?."
-          btnType="Secondary"
-          slug="test-project"
-        />
-        <Card
-          type="Project"
-          img="/hero.png"
-          alt="Srot READ Project"
-          title="Srot READ Project"
-          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil deleniti incidunt vero facere vitae quod commodi amet? Animi, esse molestias. Nihil deleniti incidunt vero facere vitae quod commodi amet? Animi, esse molestias."
-          btnType="Secondary"
-          slug="test-project"
-        />
+        {projects.map(({ slug, entry }, i) => {
+          const { title, description, image, imageAlt } = entry
+          return (
+            <Card
+              key={slug}
+              type="Project"
+              img={image}
+              alt={imageAlt}
+              title={title}
+              description={description}
+              btnType={i === 0 ? "Primary" : "Secondary"}
+              slug={slug}
+            />
+          )
+        })}
       </div>
     </main>
   )
