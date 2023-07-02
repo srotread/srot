bde68af76e87a3e8fc1938b8e26a34a3da5672c2
Neesh <avaneeshsamsi@gmail.com>
2023-07-02 18:39:26 +0530

    feat: Add dynamic urls

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,12 +1,35 @@
 import { MetadataRoute } from "next"
 import * as config from "@/lib/config"
+import reader from "@/lib/keystatic"
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const { pages } = config
   const pageUrls = pages.map((page) => ({
     url: `${config.url}${page.path}`,
     lastModified: new Date(),
   }))
+  const projectsUrls = (await reader.collections.projects.list()).map(
+    (path) => ({
+      url: `${config.url}/projects/${path}`,
+      lastModified: new Date(),
+    })
+  )
+  const workshopsUrls = (await reader.collections.workshops.list()).map(
+    (path) => ({
+      url: `${config.url}/workshops/${path}`,
+      lastModified: new Date(),
+    })
+  )
+  const centresUrls = (await reader.collections.centres.list()).map((path) => ({
+    url: `${config.url}/centres/${path}`,
+    lastModified: new Date(),
+  }))
 
-  return [...pageUrls, { url: `${config.url}/support` }]
+  return [
+    ...pageUrls,
+    ...projectsUrls,
+    ...workshopsUrls,
+    ...centresUrls,
+    { url: `${config.url}/support` },
+  ]
 }
