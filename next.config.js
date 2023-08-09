441e5237c8e2d3b93c3f826872ee9b2e6f9135a1
Neesh <avaneeshsamsi@gmail.com>
2023-08-09 12:31:08 +0530

    feat: Add redirect to analytics

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/stats",
+        destination:
+          "https://neesh-umami.vercel.app/websites/13d7f4ef-b903-4188-b145-a416895e10ab",
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
