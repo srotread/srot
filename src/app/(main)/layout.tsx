cc82b62a54cccd17455f12b98891edc2e2ac4ed3
NeeshSamsi <avaneeshsamsi@gmail.com>
2024-03-20 17:53:20 +0530

    feat: Next viewport meta export

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -76,7 +76,6 @@ export default function MainLayout({
     <html lang="en">
       <head>
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="author" content="The Srot Foundation" />
       </head>
       <Script
